aea03dccf8c828018ae09ae1d50adb3f59f6af2a
Roy van de Water <roy.vwater@gmail.com>
2015-09-05 11:59:01 -0700

    Working solution using iteration

--- challenge1/challenge1.go
+++ challenge1/challenge1.go
@@ -2,5 +2,18 @@ package challenge1
 
 // Run calculates the nth value of the Fibonacci Sequence
 func Run(n int) uint64 {
-	return 0
+	sequence := []uint64{0, 1}
+
+	if n < 2 {
+		return sequence[n]
+	}
+
+	for i := 2; i <= n; i++ {
+		lastValue := sequence[len(sequence)-1]
+		secondToLastValue := sequence[len(sequence)-2]
+
+		sequence = append(sequence, lastValue+secondToLastValue)
+	}
+
+	return sequence[len(sequence)-1]
 }
