a8e545a5ea2851e2fefc3cad9f78cc963f583fa4
Roy van de Water <roy.vwater@gmail.com>
2015-09-06 08:56:19 -0700

    Please don't ask for the 101st number <_<

--- challenge1/challenge1.go
+++ challenge1/challenge1.go
@@ -1,6 +1,25 @@
 package challenge1
 
+var sequence []uint64
+
 // Run calculates the nth value of the Fibonacci Sequence
 func Run(n int) uint64 {
-	return 0
+	sequence := generate()
+	return sequence[n]
+}
+
+func generate() []uint64 {
+	if sequence != nil {
+		return sequence
+	}
+	sequence = []uint64{0, 1}
+
+	for i := 2; i <= 100; i++ {
+		lastValue := sequence[len(sequence)-1]
+		secondToLastValue := sequence[len(sequence)-2]
+
+		sequence = append(sequence, lastValue+secondToLastValue)
+	}
+
+	return sequence
 }
