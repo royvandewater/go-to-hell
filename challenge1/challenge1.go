05e31352c4dac4f28353bb5c090cc544d33224e5
Roy van de Water <roy.vwater@gmail.com>
2015-09-06 08:15:16 -0700

    Returning the second to last number may be too clever

--- challenge1/challenge1.go
+++ challenge1/challenge1.go
@@ -2,5 +2,14 @@ package challenge1
 
 // Run calculates the nth value of the Fibonacci Sequence
 func Run(n int) uint64 {
-	return 0
+	return fib(n, 0, 1)
+}
+
+func fib(n int, secondToLastNumber, lastNumber uint64) uint64 {
+	if n == 0 {
+		return secondToLastNumber
+	}
+
+	number := secondToLastNumber + lastNumber
+	return fib(n-1, lastNumber, number)
 }
