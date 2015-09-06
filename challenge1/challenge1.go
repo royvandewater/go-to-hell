2d4a3fc7ca65d3b40fcc3af13c489b3c6491dc8d
Roy van de Water <roy.vwater@gmail.com>
2015-09-06 08:37:46 -0700

    Even the Go canonical version isn't that great

--- challenge1/challenge1.go
+++ challenge1/challenge1.go
@@ -1,6 +1,20 @@
 package challenge1
 
+import "math/big"
+
 // Run calculates the nth value of the Fibonacci Sequence
 func Run(n int) uint64 {
-	return 0
+	// Initialize two big ints with the first two numbers in the sequence.
+	a := big.NewInt(0)
+	b := big.NewInt(1)
+
+	// Loop while a is smaller than 1e100.
+	for i := 0; i < n; i++ {
+		// Compute the next Fibonacci number, storing it in a.
+		a.Add(a, b)
+		// Swap a and b so that b is the next number in the sequence.
+		a, b = b, a
+	}
+
+	return a.Uint64()
 }
