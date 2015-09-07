1b284fbcb5eb7bb5f76e6568926b290c60f5c73f
Roy van de Water <roy.vwater@gmail.com>
2015-09-07 13:44:33 -0700

    Working solution

--- challenge2/challenge2.go
+++ challenge2/challenge2.go
@@ -1,6 +1,28 @@
 package challenge2
 
+import "math"
+import "math/big"
+
 // Run calculates the if n is nearly prime
 func Run(n int) bool {
-	return true
+	largestPossibleMultiple := int(math.Sqrt(float64(n)))
+
+	for i := 2; i <= largestPossibleMultiple; i++ {
+		if n%i != 0 {
+			continue
+		}
+
+		if !prime(i) {
+			continue
+		}
+
+		if prime(n / i) {
+			return true
+		}
+	}
+	return false
+}
+
+func prime(n int) bool {
+	return big.NewInt(int64(n)).ProbablyPrime(0)
 }
