87da63188be2e5519da778f009eff1bf0dfeaa12
Roy van de Water <roy.vwater@gmail.com>
2015-09-07 14:04:50 -0700

    Complete the whole factorization

--- challenge2/challenge2.go
+++ challenge2/challenge2.go
@@ -1,6 +1,30 @@
 package challenge2
 
+import "math"
+import "math/big"
+
 // Run calculates the if n is nearly prime
 func Run(n int) bool {
-	return true
+	factors := factor(n)
+	return len(factors) == 2
+}
+
+func factor(n int) []int {
+	largestPossibleMultiple := int(math.Sqrt(float64(n)))
+
+	for i := 2; i <= largestPossibleMultiple; i++ {
+		if n%i != 0 {
+			continue
+		}
+
+		if prime(i) {
+			return append(factor(n/i), i)
+		}
+	}
+
+	return []int{n}
+}
+
+func prime(n int) bool {
+	return big.NewInt(int64(n)).ProbablyPrime(0)
 }
