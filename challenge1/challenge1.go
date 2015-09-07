3c1c0c50bf959ab71772caa768ae118e78e6e574
Roy van de Water <roy.vwater@gmail.com>
2015-09-07 07:49:02 -0700

    so close

--- challenge1/challenge1.go
+++ challenge1/challenge1.go
@@ -1,6 +1,13 @@
 package challenge1
 
+import "math"
+
+const phi = -1 / math.Phi
+
 // Run calculates the nth value of the Fibonacci Sequence
 func Run(n int) uint64 {
-	return 0
+	floatN := float64(n)
+	numerator := math.Pow(math.Phi, floatN) - math.Pow(phi, floatN)
+	denominator := math.Sqrt(5)
+	return uint64(numerator / denominator)
 }
