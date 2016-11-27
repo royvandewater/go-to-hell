83a99bc8248afee5cc8d631d3f0778a415ac9239
Roy van de Water <roy.v.water@gmail.com>
2016-11-27 21:23:27 +0000

    standard recursion

--- challenge1/challenge1.go
+++ challenge1/challenge1.go
@@ -2,5 +2,16 @@ package challenge1
 
 // Run calculates the nth value of the Fibonacci Sequence
 func Run(n int) uint64 {
-	return 0
+  if n == 0 || n == 1 {
+    return uint64(n)
+  }
+	return fib(n-2, 1, 0)
 }
+
+func fib(n int, n1, n2 uint64) uint64 {
+  if n == 0 {
+    return n1 + n2
+  }
+  return fib(n-1, n1+n2, n1)
+}
+
